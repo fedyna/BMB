4b94ae6b4b21766e224f9f80467fd9265ebfdbd5
Sergey Cherkasov <remoteproduction@yandex.ru>
2016-06-17 17:37:37 +0300

    Feature engineering begins

--- features.R
+++ features.R
@@ -11,7 +11,8 @@ load("reduced.Rdata")
 ################################ ATTENTION!!!! ################################
 # Use fread in case of full data set!
 # dt <- fread('train.csv', 
-        #colClasses=c(Cliente_ID="numeric",Producto_ID="numeric",Demanda_uni_equil="numeric"))
+        #colClasses=c(Cliente_ID="numeric",
+        #Producto_ID="numeric",Demanda_uni_equil="numeric"))
 dt <- setDT(a)
 # Colums to integer and setting keys
 dt <- dt[, `:=` (Cliente_ID = as.double(Cliente_ID), 
@@ -28,7 +29,8 @@ test <- dt[Semana == 9]
 median <- train[, median(Demanda_uni_equil)]
 median_Prod <- train[, median(Demanda_uni_equil), by = Producto_ID]
 setnames(median_Prod,"V1","M2")
-median_Client_Prod <- train[, as.double(median(Demanda_uni_equil)),by = .(Producto_ID,Cliente_ID)]
+median_Client_Prod <- train[, as.double(median(Demanda_uni_equil)), 
+                            by = .(Producto_ID, Cliente_ID)]
 setnames(median_Client_Prod,"V1","M3")
 submit <- merge(test, median_Client_Prod, all.x = TRUE)
 submit$M2 <- merge(test, median_Prod, by = "Producto_ID", all.x = TRUE)$M2
@@ -38,4 +40,23 @@ submit[is.na(Pred)]$Pred <- median
 # Let us see error
 e <- sqrt(mean((log(submit$Pred + 1) - log(submit$Demanda_uni_equil + 1))^2))
 # 0.68743
+
+############################ SECOND FEATURE - PRICE ##########################
+
+train[, Price := Venta_hoy/Venta_uni_hoy]
+# Let us check do all products numbers have unique price.
+num.of.products <- length(unique(train$Producto_ID))
+pairs <- train[, .(Producto_ID, Price)]
+pairs <- unique(pairs)
+num.of.pairs <- nrow(pairs)
+num.of.pairs == num.of.products # !!!! FALSE !!!!!!!
+# OOOOOOOHHHHHHHH!!!!!!!! let's adjust it.
+
+pairs <- pairs[,.(Prezzo = median(Price, na.rm = T)), by = Producto_ID]
+train <- merge(train, pairs, by = "Producto_ID", all.x = T)
+train <- train[, Price := NULL]
+test <- merge(test, pairs, by = "Producto_ID", all.x = T)
+
+
+
 
---
+++ products_munging.R
@@ -0,0 +1,57 @@
+
+products <- read.csv("../producto_tabla.csv", stringsAsFactors = F)
+
+# Last numbers in NombreProducto look like Producto_ID
+# Let's check
+last.numbers <- sapply(products$NombreProducto, 
+                       function(x) tail(unlist(strsplit(x, " ")), 1))
+last.numbers <- as.integer(last.numbers)
+sum(last.numbers - products$Producto_ID) 
+# ZERO! So we can just remove all those last numbers
+products$NombreProducto <- gsub(' [0-9]+$', '', products$NombreProducto)
+
+# Now let us have a look on those capital letters at the end of lines
+# We put them into separate column and remove from the line
+products$CAP.LETTES <- regmatches(products$NombreProducto, 
+                                  gregexpr("[[:upper:]* [[:upper:]]*$", 
+                                           products$NombreProducto))
+# Remove leading space
+products$CAP.LETTES <- sapply(products$CAP.LETTES, 
+                              function (x)  sub("^\\s+", "", x))
+# Remove from NombreProducto
+products$NombreProducto <- gsub("[[:upper:]* [[:upper:]]*$", "",
+                                products$NombreProducto)
+
+
+# OK, now let us separate weights and its measurements
+# We use as.character to unlist result of regmatches
+products$Weights <- as.character(regmatches(products$NombreProducto,
+                             gregexpr(" [0-9]+g| [0-9]+Kg| [0-9]+kg| [0-9]+ml| [0-9]+gProm",
+                                      products$NombreProducto)))
+
+
+# Now let us separate measurements
+products$Weights.measure <- gsub(" [0-9]+", "", products$Weights)
+
+unique(products$Weights.measure)
+#[1] "character(0)"    "g"      "Kg"     "ml"        "kg"     "c(\"g\", \"g\")"
+#[7] "gProm"
+# Dirty result! Let us clean it step by step
+##1
+products$Weights.measure[which(products$Weights.measure == "gProm")] <- "g"
+##2
+products$Weights.measure[which(products$Weights.measure == "Kg")] <- "kg"
+##3
+products[which(products$Weights.measure == "c(\"g\", \"g\")"),]
+#412       30302 Tostado 210g y Cajeta Quemada 18g        BIM c(" 210g", " 18g")     c("g", "g")
+#obviously, Watson!
+products[which(products$Weights.measure == 
+                   "c(\"g\", \"g\")"),]$Weights <- "210g"
+products[which(products$Weights.measure == 
+                   "c(\"g\", \"g\")"),]$Weights.measure <- "g"
+
+nrow(products[which(products$Weights.measure == "character(0)"),])
+# [1] 64 !!!!! What are they?
+products[which(products$Weights.measure == "character(0)"),]
+
+#### PLAY MORE WITH REGEXP!!
