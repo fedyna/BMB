4a8c38618b8c99f42e7e5d36f4e9a33bd152f470
Sergey Cherkasov <remoteproduction@yandex.ru>
2016-06-23 01:00:04 +0300

    price feature added

---
+++ f3_prices.R
@@ -0,0 +1,27 @@
+# Libraries
+libs <- c("data.table")
+sapply(libs, library, character.only = T, logical.return = T, 
+       quietly = T, warn.conflicts = F)
+
+# Uploading small data
+load("f2_reduced.Rdata")
+
+dt[, Price := Venta_hoy/Venta_uni_hoy]
+# Let us check do all products numbers have unique price.
+num.of.products <- length(unique(dt$Producto_ID))
+pairs <- dt[, .(Producto_ID, Price)]
+pairs <- unique(pairs)
+num.of.pairs <- nrow(pairs)
+num.of.pairs == num.of.products # !!!! FALSE !!!!!!!
+# OOOOOOOHHHHHHHH!!!!!!!! let's adjust it.
+
+pairs <- pairs[,.(Prezzo = median(Price, na.rm = T)), by = Producto_ID]
+dt <- merge(dt, pairs, by = "Producto_ID", all.x = T)
+dt <- dt[, Price := NULL]
+
+save(dt, file = "f3_reduced.RData")
+
+
+
+
+
