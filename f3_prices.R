b5410d5f55829dd10e6934142048586ea1ca5bc6
Sergey Cherkasov <remoteproduction@yandex.ru>
2016-06-24 14:18:33 +0300

    Battle with NA

--- f3_prices.R
+++ f3_prices.R
@@ -7,12 +7,14 @@ sapply(libs, library, character.only = T, logical.return = T,
 load("f2_reduced.Rdata")
 
 dt[, Price := Venta_hoy/Venta_uni_hoy]
+dt[is.na(dt$Price), Price := Dev_proxima/Dev_uni_proxima]
+dt[is.na(dt$Price), Price := 0]
 # Let us check do all products numbers have unique price.
-num.of.products <- length(unique(dt$Producto_ID))
+num.of.products <- length(unique(dt$Producto_ID))#DO NOT RUN ON BIG SET
 pairs <- dt[, .(Producto_ID, Price)]
 pairs <- unique(pairs)
-num.of.pairs <- nrow(pairs)
-num.of.pairs == num.of.products # !!!! FALSE !!!!!!!
+num.of.pairs <- nrow(pairs)#DO NOT RUN ON BIG SET
+num.of.pairs == num.of.products # !!!! FALSE !!!!!!!#DO NOT RUN ON BIG SET
 # OOOOOOOHHHHHHHH!!!!!!!! let's adjust it.
 
 pairs <- pairs[,.(Prezzo = median(Price, na.rm = T)), by = Producto_ID]
